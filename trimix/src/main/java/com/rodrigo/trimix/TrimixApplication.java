fc9d95d787f0a2b4b181da15acf4a49cdb9c6fcb
LeivaRodrigo <leivasebastian98@gmail.com>
2023-05-21 19:53:06 -0300

    Inicializacion Proyecto

---
+++ trimix/src/main/java/com/rodrigo/trimix/TrimixApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.trimix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimixApplication.class, args);
+	}
+
+}
